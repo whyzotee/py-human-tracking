dbb2f103e46fd4caea4fe71c2c77c74ad9c656d8
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2023-12-08 00:13:02 +0700

    feat: update

--- human_tracking.py
+++ human_tracking.py
@@ -3,42 +3,26 @@ import requests
 import numpy as np
 import cvlib as cv
 import urllib.request
-
-from ultralytics import YOLO
 from cvlib.object_detection import draw_bbox
 
-url = 'http://192.4.0.1/stream'
-led_on = "http://192.4.0.1/led_on"
-led_off = "http://192.4.0.1/led_off"
-
+url = 'http://192.168.31.36/stream'
+led_on = "http://192.168.31.36/led_on"
+led_off = "http://192.168.31.36/led_off"
 
 def detect_people(image):
-    bbox, label, conf = cv.detect_common_objects(image, model="yolov3")
-    draw_bbox(image, bbox, label, conf)
-    return label
-
-def detect_people_new(image):
-    model = YOLO("yolov8n.yaml")
-    model = YOLO("yolov8n.pt")
-    bbox, label, conf = cv.detect_common_objects(image, model=model)
+    bbox, label, conf = cv.detect_common_objects(image, model="yolov3-tiny")
     draw_bbox(image, bbox, label, conf)
     return label
 
 def main():
     cv2.namedWindow("Hello World", cv2.WINDOW_NORMAL)
-    fd = open('./profile.png','rb')
-    img_str = fd.read()
-    fd.close()
-    while True:
-        # img_resp = urllib.request.urlopen(url)
-        # img_np = np.array(bytearray(img_resp.read()), dtype=np.uint8)
-        #  img_resp = Image.open("./profile.png",mode='r')
-        im = cv2.imread("./profile.png")
 
-        img_np = np.fromstring(img_str, np.uint8)
+    while True:
+        img_resp = urllib.request.urlopen(url)
+        img_np = np.array(bytearray(img_resp.read()), dtype=np.uint8)
         img = cv2.imdecode(img_np, -1)
 
-        lables = detect_people_new(img)
+        lables = detect_people(img)
 
         cv2.imshow('Hello World', img)
 
--- main.py
+++ main.py
@@ -1,37 +1,61 @@
 import cv2
 import datetime
+import numpy as np
+import urllib.request
 from ultralytics import YOLO
 
+url = 'http://192.168.31.36/stream'
+led_on = "http://192.168.31.36/led_on"
+led_off = "http://192.168.31.36/led_off"
+
 video_cap = cv2.VideoCapture("./test_vdo.mp4")
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov5nu.pt")
+
+mode = 0  # 0 image from url, 1 test video
+
+# while((1 + 1) == 2):
+#     imageURL = urllib.request.urlopen(url)
+#     imageByte = np.asarray(bytearray(imageURL.read()), dtype=np.uint8)
+#     imgProc = cv2.imdecode(imageByte, -1)
+#     results = model(imgProc)[0]
+#
+#     print(results)
+#
+#     cv2.imshow('Test Image Proc', imgProc)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
 while((1 + 1) == 2):
     start = datetime.datetime.now()
-    ret, frame = video_cap.read()
-    
-    if not ret:
-        break
-    
-    detactions = model(frame)[0]
-    
+
+    if mode != 0:
+        ret, imgProc = video_cap.read()
+        detactions = model(imgProc)[0]
+    else:
+        imageURL = urllib.request.urlopen(url)
+        imageByte = np.asarray(bytearray(imageURL.read()), dtype=np.uint8)
+        imgProc = cv2.imdecode(imageByte, -1)
+        detactions = model(imgProc)[0]
+
     for data in detactions.boxes.data.tolist():
         confidencs = data[4]
-        
+
         if float(confidencs) < 0.8:
             continue
-        
+
         xmin, ymin, xman, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
-        cv2.rectangle(frame, (xmin, ymin), (xman, ymax), (0, 0, 255), 2)
+        cv2.rectangle(imgProc, (xmin, ymin), (xman, ymax), (0, 0, 255), 2)
     
     end = datetime.datetime.now()
     total = (end - start).total_seconds()
     print(f"Time to process 1 frame: {total * 1000:.0f} millisec.")
-    print(f"FPS: {1/total:.2f}")
-    
+
     fps = f"FPS: {1/total:.2f}"
-    # cv2.putText(frame, fps, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8)
+    print(fps)
     
-    # cv2.imshow("Frame", frame)
+    cv2.putText(imgProc, fps, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8)
+
+    cv2.imshow("Detect human.exe", imgProc)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
