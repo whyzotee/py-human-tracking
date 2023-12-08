41e213c7ab7357dcac6162118f55b69b827d67ea
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2023-12-08 17:31:12 +0700

    feat: optimize eiei lnwza 007

---
+++ main-pi.py
@@ -0,0 +1,31 @@
+import cv2
+import requests
+import numpy as np
+from ultralytics import YOLO
+
+url = 'http://192.168.31.36/stream'
+led_on = "http://192.168.31.36/led_on"
+led_off = "http://192.168.31.36/led_off"
+
+isled_on = False
+
+model = YOLO("yolov5nu.pt")
+
+while (1 + 1) == 2:
+    imageURL = requests.get(url)
+    imageArray = np.frombuffer(bytearray(imageURL.content), dtype=np.uint8)
+    imgDecode = cv2.imdecode(imageArray, cv2.IMREAD_COLOR)
+
+    results = model.predict(source=imgDecode, imgsz=320)[0]
+    data = results.boxes.data.tolist()
+
+    if len(data) > 0 and data[0][5] == 0.0:
+        if data[0][4] < 0.8:
+            continue
+        if not isled_on:
+            requests.get(led_on)
+            isled_on = True
+    else:
+        if isled_on:
+            requests.get(led_off)
+            isled_on = False
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cv2
-import time
-# import datetime
+import datetime
 import requests
 import numpy as np
 from ultralytics import YOLO
@@ -14,25 +13,28 @@ isled_on = False
 model = YOLO("yolov5nu.pt")
 
 while (1 + 1) == 2:
-    start_time = time.time()
-    # start = datetime.datetime.now()
+    start = datetime.datetime.now()
 
     imageURL = requests.get(url)
     imageArray = np.frombuffer(bytearray(imageURL.content), dtype=np.uint8)
     imgDecode = cv2.imdecode(imageArray, cv2.IMREAD_COLOR)
 
-    detactions = model(imgDecode)[0]
+    # detactions = model(imgDecode)[0]
+    detactions = model.predict(source=imgDecode, imgsz=320)[0]
 
-    # for data in detactions.boxes.data.tolist():
-    #     confidencs = data[4]
-    #
-    #     if float(confidencs) < 0.2 or float(data[5]) != 0.0:
-    #         continue
-    #
-    #     xmin, ymin, xman, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
-    #     cv2.rectangle(imgDecode, (xmin, ymin), (xman, ymax), (0, 0, 255), 2)
+    for data in detactions.boxes.data.tolist():
+        confidencs = data[4]
+
+        if float(confidencs) < 0.8 or float(data[5]) != 0.0:
+            continue
+
+        xmin, ymin, xman, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
+        cv2.rectangle(imgDecode, (xmin, ymin), (xman, ymax), (0, 0, 255), 2)
 
     if len(detactions.boxes.data.tolist()) > 0 and detactions.boxes.data.tolist()[0][5] == 0.0:
+        if detactions.boxes.data.tolist()[0][4] < 0.8:
+            continue
+
         if not isled_on:
             requests.get(led_on)
             isled_on = True
@@ -41,18 +43,18 @@ while (1 + 1) == 2:
             requests.get(led_off)
             isled_on = False
 
-    # end = datetime.datetime.now()
-    # total = (end - start).total_seconds()
-    # print(f"Time to process 1 frame: {total * 1000:.0f} millisec.")
-    time.sleep(max(0, 1 - (time.time() - start_time)))
-    # fps = f"FPS: {1 / total:.2f}"
-    # print(fps)
-    #
-    # cv2.putText(imgDecode, fps, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8)
+    end = datetime.datetime.now()
+    total = (end - start).total_seconds()
+    print(f"Time to process 1 frame: {total * 1000:.0f} millisec.")
 
-    # cv2.imshow("Detect human.exe", imgDecode)
+    fps = f"FPS: {1 / total:.2f}"
+    print(fps)
 
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
+    cv2.putText(imgDecode, fps, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 8)
 
-# cv2.destroyAllWindows()
+    cv2.imshow("Detect human.exe", imgDecode)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
